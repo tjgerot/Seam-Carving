f07e8fe7b5cffecd0e9a3b752aa0df4f06f1e820
Thomas Gerot <tjgerot@cox.net>
2016-02-26 23:16:22 -0600

    Added in pixel value getterwq

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from PIL import Image
 def main():
-  print("Hello World")
-  print("Connecting to git")
+	print("Starting Program...")
+	def getPixel(x,y):
+		img = Image.open("images/lake.jpg")
+		px = img.load()
+		r,g,b = px[x,y]
+		return (str(r) + "," + str(g) + "," + str(b))
+	#height = Images.getHeight();
+	#width = Images.getWidth();
+	print(getPixel(5,88))
 if __name__ == "__main__":
   main()
