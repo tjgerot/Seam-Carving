5164174c660916e865b97ce462774ffdc9efd26a
Thomas Gerot <tgerot179@millardps.org>
2016-02-28 12:42:45 -0600

    Finished intial method

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def energy(pxls,x,y):
 	rr,rg,rb = pxls[(x+1),y]
 	tr,tg,tb = pxls[x,(y+1)]
 	br,bg,bb = pxls[x,(y-1)]
+	e = ((pow(abs(lr - rr),2)) + (pow(abs(lg - rg),2)) + (pow(abs(lb - rb),2))) + ((pow(abs(tr - br),2)) + (pow(abs(tg - bg),2)) + (pow(abs(tb - bb),2)))
+	return e
 
 def getImage(filename):
 	img = Image.open(filename)
@@ -19,7 +21,7 @@ def main():
 	print("Starting Program...")
 	img = getImage("images/lake.jpg")
 	pxls = getPixels(img)
-	energy(pxls,50,50)
+	print(energy(pxls,50,50))
 
 if __name__ == "__main__":
   main()
