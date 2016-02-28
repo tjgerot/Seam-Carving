ac2814838e4a8290ef1336691034ec751b4070be
Thomas Gerot <tjgerot@cox.net>
2016-02-28 12:59:58 -0600

    Finished energy calc

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from PIL import Image
 def energy(pxls,x,y):
-	cr,cg,cb = pxls[x,y]
-	lr,lg,lb = pxls[(x-1),y]
-	rr,rg,rb = pxls[(x+1),y]
-	tr,tg,tb = pxls[x,(y+1)]
-	br,bg,bb = pxls[x,(y-1)]
 	e = ((pow(abs((pxls[(x-1),y][0]) - (pxls[(x+1),y][0])),2)) + (pow(abs((pxls[(x-1),y][1]) - (pxls[(x+1),y][1])),2)) + (pow(abs((pxls[(x-1),y][2]) - (pxls[(x+1),y][2])),2))) + ((pow(abs((pxls[x,(y+1)][0]) - (pxls[x,(y-1)][0])),2)) + (pow(abs((pxls[x,(y+1)][1]) - (pxls[x,(y-1)][1])),2)) + (pow(abs((pxls[x,(y+1)][2]) - (pxls[x,(y-1)][2])),2)))
 	return e
 
